b58cf83c1f891734553610f9a9fa5a71451a2573
Asad Khan <mrasad10khan@gmail.com>
2024-09-07 15:11:22 +0500

    Creating Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar";
+import Home from "./Components/Home/Home";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
       <Navbar />
+      <Home />
     </>
   );
 }
