9628108c27aeb0ed37a4541be585f95b24f412c1
Asad Khan <mrasad10khan@gmail.com>
2024-09-09 00:11:51 +0500

    Adding features to Event Page

--- src/Components/Events/Data.js
+++ src/Components/Events/Data.js
@@ -8,18 +8,27 @@ const data = [
     img: img1,
     text: "loremvjlslknvkldsnlvnlsdnv slmn vlksn  sv dlkvklsv lk klnklndvklnl ls kl n slk kskl klsnkldnvlk slkkl",
     price: "$599",
+    per: "day",
+    views: "18k",
+    likes: "2k",
   },
   {
     header: "Customized Group Hiking",
     img: img2,
-    text: "loremvjlslknvkldsnlvnlsdnv slmn vlksn  sv dlkvklsv lk klnklndvklnl ls kl n slk kskl klsnkldnvlk slkkl",
+    text: "loremvjl loremvjlslknvkldsnlvnlsdnvloremvjlslknvkldsnlvnlsdnv loremvjlslknvkldsnlvnlsdnvslmn vlksn  sv dlkvklsv lk klnklndvklnl ls kl n slk kskl klsnkldnvlk slkkl",
     price: "$399",
+    per: "group",
+    views: "5k",
+    likes: "753",
   },
   {
     header: "Drift and Flow",
     img: img3,
     text: "loremvjlslknvkldsnlvnlsdnv slmn vlksn  sv dlkvklsv lk klnklndvklnl ls kl n slk kskl klsnkldnvlk slkkl",
     price: "$99",
+    per: "person",
+    views: "2k",
+    likes: "95",
   },
 ];
 
--- src/Components/Events/Events.jsx
+++ src/Components/Events/Events.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Events.css";
 import data from "./Data";
+import leaf from "./Images/leaf.jpg";
 
 function Events() {
   return (
@@ -10,13 +11,22 @@ function Events() {
           return (
             <div className="event-info-container" key={index}>
               <img src={datas.img} className="event-img" />
+
               <div className="event-info">
                 <h2 className="event-header">{datas.header}</h2>
-                <p className="event-text">{datas.text}</p>
 
+                <div className="header-container">
+                  <p className="event-text">{datas.text}</p>
+                  <div className="views-container">
+                    <p className="event-views">{datas.views} views</p>
+                    <p className="event-likes">{datas.likes} likes</p>
+                  </div>
+                </div>
                 <div className="btn-prices">
                   <button className="event-btn">See Details</button>
-                  <p className="event-price">Starts at {datas.price}</p>
+                  <p className="event-price">
+                    Starts at <strong>{datas.price}</strong>/{datas.per}
+                  </p>
                 </div>
               </div>
             </div>
