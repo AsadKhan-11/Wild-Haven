4ccb62baffcb813b410de41d5bb6e0d1f6922374
Asad Khan <mrasad10khan@gmail.com>
2024-09-12 19:25:53 +0500

    Fixing navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
 import "./Navbar.css";
 function Navbar() {
+  const [navbarBackground, setNavbarBackground] = useState(false);
+
+  const handleScroll = () => {
+    if (window.scrollY > 100) {
+      setNavbarBackground(true);
+    } else {
+      setNavbarBackground(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <nav className="navbar">
+    <nav className={navbarBackground ? "navbar scrolled" : "navbar"}>
       <h1 className="nav-header">Wild Haven</h1>
       <div className="links">
         <a href="#Camp" className="nav-link">
