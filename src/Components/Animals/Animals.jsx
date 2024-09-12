ace54679f855ecdee99237ed622ad8beab284af3
Asad Khan <mrasad10khan@gmail.com>
2024-09-12 17:05:26 +0500

    Adding button functionalities

--- src/Components/Animals/Animals.jsx
+++ src/Components/Animals/Animals.jsx
@@ -22,21 +22,49 @@ function Animals() {
     <div className="animals">
       <div className="animals-container">
         <div className="animals-info">
+          {Names.map((name, index) => (
+            <div
+              className={`animals-icon ${
+                index === currentIndex ? "active" : ""
+              }`}
+              key={index}
+            >
+              <img
+                src={name.icon}
+                alt={`animal-${index}`}
+                className="animals-icon-img"
+              />
+            </div>
+          ))}
           <h2 className="animals-header">Discover Captivating Wildlife</h2>
           <p className="animals-txt">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda
-            iusto quisquam molestias laborum beatae obcaecati voluptatum totam,
-            corrupti optio deserunt repudiandae similique tenetur, corporis,
-            exercitationem tempora tempore non dolorem? Exercitationem, cumque
-            labore mollitia quam officia dicta perspiciatis assumenda, ab sunt
-            ullam at necessitatibus iste quasi doloribus quo recusandae tenetur
-            odit.
+            Step into the heart of the savannah and come face-to-face with the
+            king of the jungle! Our pride of lions roam with a regal confidence,
+            their golden manes glistening in the sun. Witness the power and
+            grace of these magnificent creatures as they patrol their territory,
+            protect their family, and showcase their primal instincts. As the
+            lion's mighty roar echoes across the park, you'll feel the true
+            essence of nature's most revered predator. A visit to our wildlife
+            park offers an unforgettable encounter with the wild, where you can
+            experience the raw beauty and untamed spirit of the animal kingdom.
           </p>
           <div className="btn-container">
-            <button className="animals-btn left-btn" onClick={prevImage}>
+            <button
+              className={`animals-btn left-btn${
+                currentIndex === 0 ? " disabled" : ""
+              }`}
+              disabled={currentIndex === 0 || Names.length === 0}
+              onClick={prevImage}
+            >
               <ion-icon name="arrow-back-outline"></ion-icon>
             </button>
-            <button className="animals-btn right-btn" onClick={nextImage}>
+            <button
+              className={`animals-btn right-btn${
+                currentIndex == Names.length - 1 ? " disabled" : ""
+              }`}
+              disabled={currentIndex === Names.length - 1}
+              onClick={nextImage}
+            >
               <ion-icon name="arrow-forward-outline"></ion-icon>
             </button>
           </div>
--- src/Components/Animals/Names.js
+++ src/Components/Animals/Names.js
@@ -2,19 +2,27 @@ import lion from "./images/lion.png";
 import tiger from "./images/tiger.png";
 import gorilla from "./images/gorilla.png";
 import bear from "./images/bear.png";
+import lionIcon from "./images/lion-icon.svg";
+import tigerIcon from "./images/tiger-icon.svg";
+import gorillaIcon from "./images/gorilla-icon.svg";
+import bearIcon from "./images/bear-icon.svg";
 
 const Names = [
   {
     img: lion,
+    icon: lionIcon,
   },
   {
     img: tiger,
+    icon: tigerIcon,
   },
   {
     img: gorilla,
+    icon: gorillaIcon,
   },
   {
     img: bear,
+    icon: bearIcon,
   },
 ];
 
