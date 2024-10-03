5d0d4181aed7bc6fd0ecd31e733b43bcd8121669
Asad Khan <mrasad10khan@gmail.com>
2024-10-03 22:08:21 +0500

    Making responsive

--- src/Components/Animals/Animals.jsx
+++ src/Components/Animals/Animals.jsx
@@ -20,68 +20,70 @@ function Animals() {
 
   return (
     <div className="animals" id="Animals">
-      <div className="animals-container">
-        <div className="animals-info">
-          {Names.map((name, index) => (
-            <div
-              className={`animals-icon ${
-                index === currentIndex ? "active" : ""
-              }`}
-              key={index}
-            >
-              <img
-                src={name.icon}
-                alt={`animal-${index}`}
-                className="animals-icon-img"
-              />
-            </div>
-          ))}
-          <h2 className="animals-header">Discover Captivating Wildlife</h2>
-          {Names.map((name, index) => (
-            <p
-              className={`animals-txt ${
-                index === currentIndex ? "active" : ""
-              }`}
-              key={index}
-            >
-              {name.txt}
-            </p>
-          ))}
-          <div className="btn-container">
-            <button
-              className={`animals-btn left-btn${
-                currentIndex === 0 ? " disabled" : ""
-              }`}
-              disabled={currentIndex === 0 || Names.length === 0}
-              onClick={prevImage}
-            >
-              <ion-icon name="arrow-back-outline"></ion-icon>
-            </button>
-            <button
-              className={`animals-btn right-btn${
-                currentIndex == Names.length - 1 ? " disabled" : ""
-              }`}
-              disabled={currentIndex === Names.length - 1}
-              onClick={nextImage}
-            >
-              <ion-icon name="arrow-forward-outline"></ion-icon>
-            </button>
-          </div>
-        </div>
-
-        <div className="slider">
-          {Names.map((name, index) => {
-            return (
+      <div className="animals-wrapper">
+        <div className="animals-container">
+          <div className="animals-info">
+            {Names.map((name, index) => (
               <div
-                className={`animals-img-container ${
+                className={`animals-icon ${
                   index === currentIndex ? "active" : ""
-                } ${direction}`}
+                }`}
                 key={index}
               >
-                <img src={name.img} className="animals-img" />
+                <img
+                  src={name.icon}
+                  alt={`animal-${index}`}
+                  className="animals-icon-img"
+                />
               </div>
-            );
-          })}
+            ))}
+            <h2 className="animals-header">Discover Captivating Wildlife</h2>
+            {Names.map((name, index) => (
+              <p
+                className={`animals-txt ${
+                  index === currentIndex ? "active" : ""
+                }`}
+                key={index}
+              >
+                {name.txt}
+              </p>
+            ))}
+            <div className="btn-container">
+              <button
+                className={`animals-btn left-btn${
+                  currentIndex === 0 ? " disabled" : ""
+                }`}
+                disabled={currentIndex === 0 || Names.length === 0}
+                onClick={prevImage}
+              >
+                <ion-icon name="arrow-back-outline"></ion-icon>
+              </button>
+              <button
+                className={`animals-btn right-btn${
+                  currentIndex == Names.length - 1 ? " disabled" : ""
+                }`}
+                disabled={currentIndex === Names.length - 1}
+                onClick={nextImage}
+              >
+                <ion-icon name="arrow-forward-outline"></ion-icon>
+              </button>
+            </div>
+          </div>
+
+          <div className="slider">
+            {Names.map((name, index) => {
+              return (
+                <div
+                  className={`animals-img-container ${
+                    index === currentIndex ? "active" : ""
+                  } ${direction}`}
+                  key={index}
+                >
+                  <img src={name.img} className="animals-img" />
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
